6cd6d06396799c8c202c3168ea5ffeef7114310e
PC <ankit.appnings@gmail.com>
2016-12-10 17:29:09 +0530

    Just Miwok

--- src/main/java/com/example/pc/newmiwok/SplashScreen.java
+++ src/main/java/com/example/pc/newmiwok/SplashScreen.java
@@ -1,6 +1,9 @@
 package com.example.pc.newmiwok;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Created by PC on 10-12-2016.
@@ -9,4 +12,22 @@ import android.app.Activity;
 public class SplashScreen extends Activity {
 
 
+    //Spalsh Screen Time out
+    private static int SPLASH_SCREEN_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_OUT);
+    }
 }
